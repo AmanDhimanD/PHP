fddb8243b6d8d5208f52271609860d0152159c4b
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-07-18 21:50:00 +0530

    Add files via upload

---
+++ 04_Data_Type.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Type</title>
+</head>
+<body>
+    <?php
+        $var="Rayne Coder";
+        echo var_dump($var);
+
+        $var=12;
+        echo "<br>";
+        echo var_dump($var);
+
+        $var=223.4545;
+        echo "<br>";
+        echo var_dump($var);
+
+        $var=true;
+        echo "<br>";
+        echo var_dump($var);
+        echo "<br>";
+        
+        define("Pi",3.14); //Constant
+        echo Pi;
+
+
+    ?>
+</body>
+</html>
