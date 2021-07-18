806723965cf617629af9bf8dc08153e9dc017ed8
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-07-18 21:49:51 +0530

    Add files via upload

---
+++ 02_Variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable</title>
+</head>
+<body>
+
+    <?php
+    echo "Variables ";
+    $variable1 =34;
+    $variable2 =23;
+    
+    echo $variable1 + $variable2;
+    
+
+    ?>
+</body>
+</html>
